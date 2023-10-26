1aa0c3ee2b156f8d85803b3a4f691f9e09a8b6b0
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-10-27 00:04:01 +0300

    Add files via upload

---
+++ square.py
@@ -0,0 +1,15 @@
+import turtle;
+
+tim= turtle.Turtle()
+tim.color('pink')
+tim.shape("turtle")
+tim.pensize(5)
+
+
+tim.forward(200)
+tim.left(90)
+tim.forward(200)
+tim.right(90)
+tim.backward(200)
+tim.right(90)
+tim.forward(200)
